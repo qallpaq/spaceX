a272181d8c32cc1f5ecee89496dd51392066f5c8
qallpaq <callpack96@gmail.com>
2020-10-20 18:44:57 +0400

    slider-dots

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -3,20 +3,25 @@ import './slider.scss';
 
 
 const Slider = ({array}) => {
-    const [x, setX] = useState(0);
+    const [x, setX] = useState({pos: 0, currentSlide: 1});
 
     const goLeft = () => {
-        x === 0 ? setX(-100 * (array.length - 1)) : setX(x + 100);
-    };
+        x.pos === 0 ? setX({pos: -100 * (array.length - 1), currentSlide: array.length})
+            : setX({pos: x.pos + 100, currentSlide: x.currentSlide - 1});
+    }
+
     const goRight = () => {
-        x === -100 * (array.length - 1) ? setX(0) : setX(x - 100);
-    };
+        x.pos === -100 * (array.length - 1) ? setX({pos: 0, currentSlide: 1})
+            : setX({pos: x.pos - 100, currentSlide: x.currentSlide + 1});
+    }
+
+    const arrayForDots = array.map((_, idx) => idx + 1);
 
     return (
         <div className='slider'>
             {array.map((el, idx) => {
                 return (
-                    <div className='slide' style={{transform: `translateX(${x}%)`}} key={idx}>
+                    <div className='slide' style={{transform: `translateX(${x.pos}%)`}} key={idx}>
                         <img src={el} alt="slider-img"/>
                     </div>
                 );
@@ -30,6 +35,20 @@ const Slider = ({array}) => {
                 <i className="fas fa-chevron-right" style={{fontSize: 40 + 'px'}}>
                 </i>
             </button>
+
+            <div className='slider-dots'>
+                {
+                    arrayForDots.map((el, idx) => {
+                        return (
+                            <button className={`slider__dot ${el === x.currentSlide && 'active'}`}
+                                    key={idx}
+                                    onClick={() => setX({pos: idx * (-100), currentSlide: el})}>
+                                {el}
+                            </button>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
