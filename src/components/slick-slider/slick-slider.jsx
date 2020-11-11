15ace13797503e85d63229eca750a8105ad81c2c
qallpaq <callpack96@gmail.com>
2020-11-11 17:36:51 +0400

    slider

--- src/components/slick-slider/slick-slider.jsx
+++ src/components/slick-slider/slick-slider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import './slick-slider.scss';
 
 
 const SlickSlider = ({arr}) => {
@@ -13,17 +14,6 @@ const SlickSlider = ({arr}) => {
         slidesToScroll: 1,
         arrows: false,
         autoplay: true,
-        appendDots: dots => (
-            <div>
-                <ul style={{marginBottom: "40px"}}> {dots} </ul>
-            </div>
-        )
-    };
-
-    const styledImg = {
-        width: 100 + '%',
-        height: 60 + 'vh',
-        cursor: 'pointer'
     };
 
     return (
@@ -31,7 +21,7 @@ const SlickSlider = ({arr}) => {
             {arr.map((el, idx) => {
                 return (
                     <div className='slide' key={idx}>
-                        <img src={el} alt="slider-img" style={styledImg}/>
+                        <img src={el} alt="slider-img"/>
                     </div>
                 );
             })}
